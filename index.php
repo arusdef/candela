105af1059eb381389f3d045cd90142be28eda7b7
lukaslukas <eiglerhardingl@gmail.com>
2019-02-26 20:14:18 -0500

    holding index

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Candela</title>
+	<style type="text/css">
+		*{
+			font-family: arial;
+			color: blue;
+		}
+	</style>
+
+</head>
+<body>
+	<h1>candela.mx, staging site</h1>
+</body>
+</html>
+
 <?php
 
-require 'kirby/bootstrap.php';
+#require 'kirby/bootstrap.php';
 
-echo (new Kirby)->render();
+#echo (new Kirby)->render();
