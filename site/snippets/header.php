f9678f222c4b9f6c2dad5a26e202861657bef4e9
lukaslukas <eiglerhardingl@gmail.com>
2019-04-02 17:08:06 -0400

    analytics added

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137570390-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-137570390-1');
+	</script>
+
 	<title><?= $site->title()->html() ?></title>
 
 	<link rel="icon" type="image/png" href="<?= $site->url() ?>/assets/favicon/favicon16.png" sizes="16x16">  
